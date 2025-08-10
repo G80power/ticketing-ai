30a881399f2d4dd4284533821bea5201863a2648
G80power <imamaan2005@gmail.com>
2025-08-10 08:45:16 +0400

    commit this changes

--- app/src/llm_factory.py
+++ app/src/llm_factory.py
@@ -9,11 +9,6 @@ from transformers import pipeline
 pipe = pipeline("text-classification", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
 # Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-model = AutoModelForSequenceClassification.from_pretrained("distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-
 
 class LLMFactory:
     def __init__(self):
